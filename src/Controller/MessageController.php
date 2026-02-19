3c79a86396d5133a2b0d59cf122a259cc54f7703
Auke <auke@skrepr.com>
2026-02-19 22:17:56 +0100

    Implement minimal create endpoint to satisfy failing test

---
+++ src/Controller/MessageController.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class MessageController
+{
+    #[Route('/api/messages', name: 'app_message_create', methods: ['POST'])]
+    public function create(): JsonResponse
+    {
+        return new JsonResponse(null, 201);
+    }
+}
