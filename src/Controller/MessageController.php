d96082ad74af020ded8141ef8a389e03523b18bb
Auke <auke@skrepr.com>
2026-02-19 22:48:34 +0100

    Refactored controller to use MessageService and moved business logic out of controller

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -9,23 +9,20 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\Message;
 use Doctrine\ORM\EntityManagerInterface;
+use App\Service\MessageService;
 
 final class MessageController
 {
+    public function __construct(
+        private readonly MessageService $service
+    ) {}
+
     #[Route('/api/messages', name: 'app_message_create', methods: ['POST'])]
-    public function create(Request $request, EntityManagerInterface $em): JsonResponse
+    public function create(Request $request): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
 
-        $message = new Message();
-        $message->setSubject($data['subject']);
-        $message->setMessage($data['message']);
-        $message->setDate(new \DateTimeImmutable($data['date']));
-        $message->setSenderName($data['senderName']);
-        $message->setType($data['type']);
-
-        $em->persist($message);
-        $em->flush();
+        $message = $this->service->create($data);
 
         return new JsonResponse([
             'id' => $message->getId(),
@@ -35,9 +32,9 @@ final class MessageController
     }
 
     #[Route('/api/messages', name: 'app_message_index', methods: ['GET'])]
-    public function index(EntityManagerInterface $em): JsonResponse
+    public function index(): JsonResponse
     {
-        $messages = $em->getRepository(Message::class)->findAll();
+        $messages = $this->service->findAll();
 
         $data = array_map(
             fn (Message $message) => [
@@ -52,9 +49,9 @@ final class MessageController
     }
 
     #[Route('/api/messages/{id}', name: 'app_message_show', methods: ['GET'])]
-    public function show(int $id, EntityManagerInterface $em): JsonResponse
+    public function show(int $id): JsonResponse
     {
-        $message = $em->getRepository(Message::class)->find($id);
+        $message = $this->service->find($id);
 
         if (!$message) {
             return new JsonResponse(null, 404);
@@ -71,42 +68,34 @@ final class MessageController
     }
 
     #[Route('/api/messages/{id}', name: 'app_message_update', methods: ['PUT'])]
-    public function update(int $id, Request $request, EntityManagerInterface $em): JsonResponse
+    public function update(int $id, Request $request): JsonResponse
     {
-        $message = $em->getRepository(Message::class)->find($id);
+        $message = $this->service->find($id);
 
         if (!$message) {
             return new JsonResponse(null, 404);
         }
 
         $data = json_decode($request->getContent(), true);
-
-        $message->setSubject($data['subject']);
-        $message->setMessage($data['message']);
-        $message->setDate(new \DateTimeImmutable($data['date']));
-        $message->setSenderName($data['senderName']);
-        $message->setType($data['type']);
-
-        $em->flush();
+        $updated = $this->service->update($message, $data);
 
         return new JsonResponse([
-            'id' => $message->getId(),
-            'subject' => $message->getSubject(),
-            'type' => $message->getType(),
+            'id' => $updated->getId(),
+            'subject' => $updated->getSubject(),
+            'type' => $updated->getType(),
         ]);
     }
 
     #[Route('/api/messages/{id}', name: 'app_message_delete', methods: ['DELETE'])]
-    public function delete(int $id, EntityManagerInterface $em): JsonResponse
+    public function delete(int $id): JsonResponse
     {
-        $message = $em->getRepository(Message::class)->find($id);
+        $message = $this->service->find($id);
 
         if (!$message) {
             return new JsonResponse(null, 404);
         }
 
-        $em->remove($message);
-        $em->flush();
+        $this->service->delete($message);
 
         return new JsonResponse(null, 204);
     }
---
+++ src/Service/MessageService.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+use App\Entity\Message;
+use Doctrine\ORM\EntityManagerInterface;
+
+final class MessageService
+{
+    public function __construct(
+        private readonly EntityManagerInterface $em
+    ) {}
+
+    public function create(array $data): Message
+    {
+        $message = new Message();
+        $message->setSubject($data['subject']);
+        $message->setMessage($data['message']);
+        $message->setDate(new \DateTimeImmutable($data['date']));
+        $message->setSenderName($data['senderName']);
+        $message->setType($data['type']);
+
+        $this->em->persist($message);
+        $this->em->flush();
+
+        return $message;
+    }
+
+    public function findAll(): array
+    {
+        return $this->em->getRepository(Message::class)->findAll();
+    }
+
+    public function find(int $id): ?Message
+    {
+        return $this->em->getRepository(Message::class)->find($id);
+    }
+
+    public function update(Message $message, array $data): Message
+    {
+        $message->setSubject($data['subject']);
+        $message->setMessage($data['message']);
+        $message->setDate(new \DateTimeImmutable($data['date']));
+        $message->setSenderName($data['senderName']);
+        $message->setType($data['type']);
+
+        $this->em->flush();
+
+        return $message;
+    }
+
+    public function delete(Message $message): void
+    {
+        $this->em->remove($message);
+        $this->em->flush();
+    }
+}
