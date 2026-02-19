6b015b2b86fc0e48b146ebf2cb0518246685bbe8
Auke <auke@skrepr.com>
2026-02-19 22:19:59 +0100

    Implemented minimal JSON handling for create endpoint

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -5,13 +5,16 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class MessageController
 {
     #[Route('/api/messages', name: 'app_message_create', methods: ['POST'])]
-    public function create(): JsonResponse
+    public function create(Request $request): JsonResponse
     {
-        return new JsonResponse(null, 201);
+        $data = json_decode($request->getContent(), true);
+
+        return new JsonResponse($data, 201);
     }
 }
